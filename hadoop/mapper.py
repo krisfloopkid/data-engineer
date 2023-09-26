e3fa8d46588c2714224a4ccc890c619c02029fad
Kristina Molodozhenova <krismolod@gmail.com>
2023-09-26 19:26:22 +0300

    Create mapper.py

---
+++ hadoop/mapper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+from datetime import datetime
+import sys
+
+
+def perform_map():
+    for line in sys.stdin:
+        data = line.strip().split(',')
+        if len(data) == 18 and data[0] != 'VendorID':
+            pickup_datetime = datetime.strptime(data[1], '%Y-%m-%d %H:%M:%S')
+            payment_type = data[9]
+            tip_amount = float(data[13])
+
+            if pickup_datetime.year == 2020 and payment_type and tip_amount >= 0:
+                month = pickup_datetime.strftime('%Y-%m')
+                print('{0},{1}]\t{2}'.format(month, payment_type, tip_amount))
+
+
+if __name__ == '__main__':
+    perform_map()
