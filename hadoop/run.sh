68cb17ea35689613cd2838aa7e366bad5739d0b6
Kristina Molodozhenova <krismolod@gmail.com>
2023-09-26 19:33:27 +0300

    Create run.sh

---
+++ hadoop/run.sh
@@ -0,0 +1,12 @@
+export MR_OUTPUT=/user/root/output-data
+
+hadoop fs -rm -r $MR_OUTPUT
+
+
+hadoop jar "$HADOOP_MAPRED_HOME"/hadoop-streaming.jar \
+-Dmapred.job.name='Taxi map-reduce job' \
+-Dmapred.reduce.tasks=1 \
+-Dmapreduce.map.memory.mb=1024 \
+-file /tmp/mapreduce/mapper.py -mapper /tmp/mapreduce/mapper.py \
+-file /tmp/mapreduce/reducer.py -reducer /tmp/mapreduce/reducer.py \
+-input /user/root/2020 -output $MR_OUTPUT
